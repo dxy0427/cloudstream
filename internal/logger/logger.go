3b1c460a7dc02471cddc427bfad8d99c58c0800e
dxy <137971972+dxy0427@users.noreply.github.com>
2025-12-08 14:15:23 +0800

    创建 internal/logger/logger.go

---
+++ internal/logger/logger.go
@@ -0,0 +1,31 @@
+package logger
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func Init() {
+	logFilePath := filepath.Join("./data", "cloudstream.log")
+
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   logFilePath,
+		MaxSize:    10,
+		MaxBackups: 3,
+		MaxAge:     28,
+		Compress:   true,
+	}
+
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}
+	multiWriter := io.MultiWriter(consoleWriter, lumberjackLogger)
+
+	log.Logger = zerolog.New(multiWriter).With().Timestamp().Logger()
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	log.Info().Str("path", logFilePath).Msg("日志系统已初始化")
+}
