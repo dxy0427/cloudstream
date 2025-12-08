df39533b6a9e4d37cb0de111072f92db51f4f3a3
dxy <137971972+dxy0427@users.noreply.github.com>
2025-12-08 19:16:29 +0800

    更新 App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <n-config-provider :theme="theme">
-    <n-global-style />
-    <n-message-provider>
-      <n-dialog-provider>
-        <router-view />
-      </n-dialog-provider>
-    </n-message-provider>
-  </n-config-provider>
+ <n-config-provider :theme="theme">
+  <n-global-style />
+  <n-message-provider>
+   <n-dialog-provider>
+    <router-view />
+   </n-dialog-provider>
+  </n-message-provider>
+ </n-config-provider>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { darkTheme } from 'naive-ui'
-import { ref } from 'vue'
+import { useGlobalStore } from './store/global'
 
-const theme = ref(darkTheme)
+const store = useGlobalStore()
+
+// 如果是深色模式，传 darkTheme；浅色模式传 null
+const theme = computed(() => store.isDark ? darkTheme : null)
 </script>
