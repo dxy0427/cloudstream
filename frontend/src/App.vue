b9bb011374bbc086c3b4e4d6250f4eb7b4d4ce97
dxy <137971972+dxy0427@users.noreply.github.com>
2025-12-08 14:23:02 +0800

    创建 frontend/src/App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-config-provider :theme="theme">
+    <n-global-style />
+    <n-message-provider>
+      <n-dialog-provider>
+        <router-view />
+      </n-dialog-provider>
+    </n-message-provider>
+  </n-config-provider>
+</template>
+
+<script setup>
+import { darkTheme } from 'naive-ui'
+import { ref } from 'vue'
+
+const theme = ref(darkTheme)
+</script>
